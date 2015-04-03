269a304e2a8ef726ad0d393492e13112c8ed3de1
DrVulpinus <siganus.vulpinus@live.com>
2015-04-03 14:37:01 -0400

    Change timer to change the text field within separate thread.  This is just better.

--- MicroThreadPrinter/src/processcontrol/Timer.java
+++ MicroThreadPrinter/src/processcontrol/Timer.java
@@ -1,21 +1,29 @@
 package processcontrol;
 
-public class Timer {
+import javax.swing.JTextField;
+
+public class Timer extends Thread{
 	private long startTime;
 	private long stopTime;
-	
+	private JTextField tf;
 	
 public Timer(){
 	
 }
+public Timer(JTextField _tf){
+	tf = _tf;
+}
 public void startTimer(){
 	startTime =  System.currentTimeMillis();
+	this.start();
 }
 public void startCountDownTimer(long _seconds){
 	startTime = System.currentTimeMillis() + (_seconds*1000);
+	this.start();
 }
 public void stopTimer(){
 	stopTime = System.currentTimeMillis();
+	this.interrupt();
 }
 public void resetTimer(){
 	startTime = 0;
@@ -31,5 +39,23 @@ public String getTimeString(){
 	long seconds = (thisTime%3600)%60;
 	outTime = hours + ":" + minutes + ":" + seconds;
 	return outTime;
+}
+
+@Override
+public void run(){
+	while(true){
+	if (tf != null){
+		tf.setText(getTimeString());
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (this.isInterrupted()){
+			return;
+		}
+	}
+	}
 }
 }
